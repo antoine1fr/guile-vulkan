6fd50dfa0e676e8109aa6a150dd5c1c85ad1962a
Antoine Luciani <antoine1fr@gmail.com>
2020-10-12 20:29:10 -0400

    Add support for linux

--- vulkan/bindings.scm
+++ vulkan/bindings.scm
@@ -1,9 +1,30 @@
 (define-module (vulkan bindings)
   #:use-module (system foreign)
+  #:use-module (ice-9 exceptions)
+  #:use-module (ice-9 format)
+  #:use-module (ice-9 match)
   #:export (handle))
 
 (eval-when (expand)
   (include-from-path "vulkan/syntax.scm"))
+
+(define (load-vulkan names)
+  (match names
+    [()
+     (let* ([error (make-external-error)]
+            [message (make-exception-with-message "can't find vulkan")]
+            [exception (make-exception error message)])
+       (raise-exception exception))]
+    [(name . tail)
+     (call/cc
+      (lambda (k)
+        (with-exception-handler
+            (lambda (x) (k (try-dynamic-link tail)))
+          (lambda ()
+            (dynamic-link name)))))]))
+
+(load-vulkan '("libMoltenVK.dylib"
+               "libvulkan"))
 
 (define lib-molten-vk
   (dynamic-link "libMoltenVK.dylib"))
